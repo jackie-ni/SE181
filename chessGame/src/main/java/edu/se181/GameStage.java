17ac95d86c622879bac0cde7ba31269d8b138f84
Jackie Ni <jnj4798@gmail.com>
2020-08-12 22:22:21 -0400

    Issue #10 Implement Stage: -Game title added as a static resource -Added newline to end of file

--- chessGame/src/main/java/edu/se181/GameStage.java
+++ chessGame/src/main/java/edu/se181/GameStage.java
@@ -1,6 +1,5 @@
 package edu.se181;
 
-import javafx.application.Application;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
@@ -8,8 +7,9 @@ import javafx.stage.Stage;
 import javafx.scene.Scene;
 
 public class GameStage {
+    private static final String CHESS_GAME = "Chess Game";
     public static void launch(Stage stage) {
-        stage.setTitle("Chess Game");
+        stage.setTitle(CHESS_GAME);
 
         GridPane chessBoard = new GridPane();
 
@@ -17,18 +17,15 @@ public class GameStage {
             for (int col = 0; col < 8; col++) {
                 Rectangle square = new Rectangle(50, 50);
 
-                Color color = Color.BLACK;
+                Color color = Color.GRAY;
                 if ((row + col) % 2 == 0) {
-                    color = Color.WHITE;
+                    color = Color.BEIGE;
                 }
                 square.setFill(color);
-
                 chessBoard.add(square, col, row);
-//                square.widthProperty().bind(ChessBoard.widthProperty().divide(8));
-//                square.heightProperty().bind(ChessBoard.heightProperty().divide(8));
             }
         }
-        Scene scene = new Scene(chessBoard, 400, 400);
+        Scene scene = new Scene(chessBoard, 500, 500);
         stage.setScene(scene);
         stage.show();
     }
