f916fd2a274cac06f4ef8c222b545b92b3ac798d
Tommy Santangelo <tommys1@comcast.net>
2020-08-23 21:49:49 -0400

    28-Implement Game Class Following merge, properly implemented the following: --getLegalMoves Extracted handleIrreversible and handleFinish out of makeMove handleIrreversible and makeMove fail to compile, waiting on merge from #30-Implement Draws handleFinish holds TODOs as server code is not yet ready Added unit tests that currently fail due to waiting on merge for handleIrreversible, handleFinish, makeMove

--- chessGame/src/main/java/edu/se181/Game.java
+++ chessGame/src/main/java/edu/se181/Game.java
@@ -9,8 +9,11 @@ public class Game {
     private boolean whiteTurn;
     private boolean playerIsWhite;
     private Socket socket;
-    private GameLogic logicUnit;
+    protected GameLogic logicUnit;
     private List<Move> moveList;
+    protected List<String> boardStates;
+    protected int halfMoveClock;
+    protected int repetitionIndex;
     private GameStage gameStage;
 
     public Game() {
@@ -18,6 +21,10 @@ public class Game {
         board.initialize();
         logicUnit = new GameLogic(board);
         moveList = new ArrayList<>();
+        boardStates = new ArrayList<>();
+        boardStates.add(board.toBoardState());
+        halfMoveClock = 0;
+        repetitionIndex = 0;
     }
 
     public Board getBoard() {
@@ -36,20 +43,61 @@ public class Game {
         return playerIsWhite;
     }
 
-    public void makeMove(Move move) {
-        moveList.add(move);
-        board.move(move);
-        /* UNCOMMENT WHEN GAMELOGIC MERGED
-        logicUnit.analyzeGameState();
+    protected void handleIrreversible(Move move) {
+        int irreversible = DrawChecker.isIrreversibleMove(move);
+        if (irreversible > 0) {
+            halfMoveClock = 0;
+        }
+        if (irreversible == 2) {
+            repetitionIndex = boardStates.size();
+        }
+    }
+
+    public void handleFinish() {
         if (logicUnit.isGameFinished()) {
+            if (logicUnit.getFinishState() == -1) {
+                // TODO: black victory
+            } else if (logicUnit.getFinishState() == 1) {
+                // TODO: white victory
+            } else {
+                if (logicUnit.getDrawCondition() == 0) {
+                    // TODO: stalemate
+                } else if (logicUnit.getDrawCondition() == 1) {
+                    // TODO: insufficient material
+                } else if (logicUnit.getDrawCondition() == 2) {
+                    // TODO: threefold repetition
+                } else if (logicUnit.getDrawCondition() == 3) {
+                    // TODO: 50 move
+                } else {
+                    // TODO: mutual draw agreement
+                }
+            }
             // TODO: implement game teardown code
         }
-         */
+    }
+
+    public void makeMove(Move move) {
+        handleIrreversible(move);
+
+        board.makeMove(move);
+
+        halfMoveClock++;
+        moveList.add(move);
+        boardStates.add(board.toBoardState());
+
+        logicUnit.analyzeGameState(isWhiteTurn(), halfMoveClock, repetitionIndex, boardStates);
+
+        handleFinish();
+
+        if (isPlayerWhite() == isWhiteTurn()) {
+            // TODO: socket code to send our move to the server
+        }
+
+        changeTurn();
     }
 
     public List<Move> getLegalMoves(int rank, int file) {
-        //Piece mover = board.getSquareByRankFile(rank, file).getOccupant();
-        //return logicUnit.getLegalMoves(mover);
-        return null; // switch out when we merge GameLogic legal moves
+        Piece mover = board.getSquareByRankFile(rank, file).getOccupant();
+        return logicUnit.getLegalMoves(mover);
     }
 }
---
+++ chessGame/src/test/java/edu/se181/GameTest.java
@@ -0,0 +1,195 @@
+package edu.se181;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class GameTest {
+    private Game game;
+
+    @Before
+    public void beforeEach() {
+        game = new Game();
+        game.boardStates.add("testBoardState1");
+        game.boardStates.add("testBoardState2");
+        game.halfMoveClock = 2;
+        game.repetitionIndex = 0;
+    }
+
+    @Test
+    public void handleIrreversible_ReversibleMove_NoCounterUpdates() {
+        Knight knight = (Knight) game.getBoard().getSquareByNotation("b1").getOccupant();
+        Move move = new RegularMove(knight, 2, 2, false);
+        game.handleIrreversible(move);
+        Assert.assertEquals(2, game.halfMoveClock);
+        Assert.assertEquals(0, game.repetitionIndex);
+    }
+
+    @Test
+    public void handleIrreversible_IrreversiblePawnMove_BothCounterUpdates() {
+        Pawn pawn = (Pawn) game.getBoard().getSquareByNotation("b2").getOccupant();
+        Move move = new RegularMove(pawn, 3, 1, false);
+        game.handleIrreversible(move);
+        Assert.assertEquals(0, game.halfMoveClock);
+        Assert.assertEquals(2, game.repetitionIndex);
+    }
+
+    @Test
+    public void handleIrreversible_IrreversibleCaptureMove_BothCounterUpdates() {
+        Knight knight = (Knight) game.getBoard().getSquareByNotation("b1").getOccupant();
+        Move move = new RegularMove(knight, 2, 2, true);
+        game.handleIrreversible(move);
+        Assert.assertEquals(0, game.halfMoveClock);
+        Assert.assertEquals(2, game.repetitionIndex);
+    }
+
+    @Test
+    public void handleIrreversible_IrreversibleCastleMove_RepetitionIndexUpdate() {
+        King king = (King) game.getBoard().getSquareByNotation("e1").getOccupant();
+        Move move = new CastleMove(king, true);
+        game.handleIrreversible(move);
+        Assert.assertEquals(0, game.halfMoveClock);
+        Assert.assertEquals(0, game.repetitionIndex);
+    }
+
+    @Test
+    public void handleIrreversible_IrreversibleRookHasNotMoved_RepetitionIndexUpdate() {
+        Rook rook = (Rook) game.getBoard().getSquareByNotation("a1").getOccupant();
+        Move move = new RegularMove(rook, 4, 0, false);
+        game.handleIrreversible(move);
+        Assert.assertEquals(0, game.halfMoveClock);
+        Assert.assertEquals(0, game.repetitionIndex);
+    }
+
+    @Test
+    public void handleIrreversible_IrreversibleKingHasNotMoved_RepetitionIndexUpdate() {
+        King king = (King) game.getBoard().getSquareByNotation("e1").getOccupant();
+        Move move = new RegularMove(king, 4, 1, false);
+        game.handleIrreversible(move);
+        Assert.assertEquals(0, game.halfMoveClock);
+        Assert.assertEquals(0, game.repetitionIndex);
+    }
+
+    @Test
+    public void handleIrreversible_IrreversibleRookHasMoved_NoCounterUpdates() {
+        Rook rook = (Rook) game.getBoard().getSquareByNotation("a1").getOccupant();
+        rook.firstMovePerformed();
+        Move move = new RegularMove(rook, 4, 0, false);
+        game.handleIrreversible(move);
+        Assert.assertEquals(2, game.halfMoveClock);
+        Assert.assertEquals(0, game.repetitionIndex);
+    }
+
+    @Test
+    public void handleIrreversible_IrreversibleKingHasMoved_NoCounterUpdates() {
+        King king = (King) game.getBoard().getSquareByNotation("e1").getOccupant();
+        king.firstMovePerformed();
+        Move move = new RegularMove(king, 4, 1, false);
+        game.handleIrreversible(move);
+        Assert.assertEquals(2, game.halfMoveClock);
+        Assert.assertEquals(0, game.repetitionIndex);
+    }
+
+    @Test
+    @Ignore
+    public void handleFinish_WhiteWins() {
+        game.logicUnit.finished = true;
+        game.logicUnit.finishState = 1;
+    }
+
+    @Test
+    @Ignore
+    public void handleFinish_BlackWins() {
+        game.logicUnit.finished = true;
+        game.logicUnit.finishState = -1;
+    }
+
+    @Test
+    @Ignore
+    public void handleFinish_DrawStalemate() {
+        game.logicUnit.finished = true;
+        game.logicUnit.finishState = 0;
+        game.logicUnit.drawCondition = 0;
+    }
+
+    @Test
+    @Ignore
+    public void handleFinish_DrawInsufficientMaterial() {
+        game.logicUnit.finished = true;
+        game.logicUnit.finishState = 0;
+        game.logicUnit.drawCondition = 1;
+    }
+
+    @Test
+    @Ignore
+    public void handleFinish_DrawRepetition() {
+        game.logicUnit.finished = true;
+        game.logicUnit.finishState = 0;
+        game.logicUnit.drawCondition = 2;
+    }
+
+    @Test
+    @Ignore
+    public void handleFinish_DrawFiftyMoveRule() {
+        game.logicUnit.finished = true;
+        game.logicUnit.finishState = 0;
+        game.logicUnit.drawCondition = 3;
+    }
+
+    @Test
+    @Ignore
+    public void handleFinish_DrawAgreement() {
+        game.logicUnit.finished = true;
+        game.logicUnit.finishState = 0;
+        game.logicUnit.drawCondition = 4;
+    }
+
+    @Test
+    public void makeMove_ReversibleMove_UpdatesInternals() {
+        boolean lastTurn = game.isWhiteTurn();
+        game.halfMoveClock = 2;
+        game.boardStates.add("state1");
+        game.boardStates.add("state2");
+        game.repetitionIndex = 0;
+        Knight knight = (Knight) game.getBoard().getSquareByNotation("b1").getOccupant();
+        Move move = new RegularMove(knight, 2, 2, false);
+        game.makeMove(move);
+        Assert.assertTrue(lastTurn != game.isWhiteTurn());
+        Assert.assertEquals(3, game.halfMoveClock);
+        Assert.assertEquals(0, game.repetitionIndex);
+        Assert.assertEquals(3, game.boardStates.size());
+    }
+
+    @Test
+    public void makeMove_SemiIrreversibleMove_UpdatesInternals() {
+        boolean lastTurn = game.isWhiteTurn();
+        game.halfMoveClock = 2;
+        game.boardStates.add("state1");
+        game.boardStates.add("state2");
+        game.repetitionIndex = 0;
+        King king = (King) game.getBoard().getSquareByNotation("e1").getOccupant();
+        Move move = new CastleMove(king, false);
+        game.makeMove(move);
+        Assert.assertTrue(lastTurn != game.isWhiteTurn());
+        Assert.assertEquals(3, game.halfMoveClock);
+        Assert.assertEquals(2, game.repetitionIndex);
+        Assert.assertEquals(3, game.boardStates.size());
+    }
+
+    @Test
+    public void makeMove_IrreversibleMove_UpdatesInternals() {
+        boolean lastTurn = game.isWhiteTurn();
+        game.halfMoveClock = 2;
+        game.boardStates.add("state1");
+        game.boardStates.add("state2");
+        game.repetitionIndex = 0;
+        Knight knight = (Knight) game.getBoard().getSquareByNotation("b1").getOccupant();
+        Move move = new RegularMove(knight, 2, 2, true);
+        game.makeMove(move);
+        Assert.assertTrue(lastTurn != game.isWhiteTurn());
+        Assert.assertEquals(1, game.halfMoveClock);
+        Assert.assertEquals(2, game.repetitionIndex);
+        Assert.assertEquals(3, game.boardStates.size());
+    }
+}
