16ebaa3639ed7b38daffafbc966414500faefb18
Tim Tran <ttran23093@gmail.com>
2020-08-23 21:03:52 -0400

    Implement Draws
    Clean up whitespace/indents

--- chessGame/src/main/java/edu/se181/DrawChecker.java
+++ chessGame/src/main/java/edu/se181/DrawChecker.java
@@ -9,104 +9,103 @@ public class DrawChecker {
     public DrawChecker(){}
 
     static public int isIrreversibleMove(Move move){
-    	Piece piece = move.getPiece();
-		if (piece instanceof Pawn || move.isCapture()){
-			return 1;
-		}
+        Piece piece = move.getPiece();
+        if (piece instanceof Pawn || move.isCapture()){
+            return 1;
+        }
 
-		if (piece instanceof Rook || piece instanceof King){
-			if (!piece.hasMoved()){
-				return 2;
-			}
-		}
+        if (piece instanceof Rook || piece instanceof King){
+            if (!piece.hasMoved()){
+                return 2;
+            }
+        }
 
-		return 0;
-	}
+        return 0;
+    }
 
-	static public boolean isThreefoldRepetition(List<String> boardStateList, int irreversibleMoveIndex){
-		HashMap<String, Integer> boardStateMap = new HashMap<>();
-		if (boardStateList.size() < 3){
-			return false;
-		}
-    	for(String boardState : boardStateList.subList(irreversibleMoveIndex, boardStateList.size()-1)){
-    		boardStateMap.putIfAbsent(boardState, 1);
-			if (boardStateMap.containsKey(boardState)){
-				if (boardStateMap.get(boardState) == 2) {
-					return true;
-				}
-				else{
-					boardStateMap.replace(boardState, boardStateMap.get(boardState)+1);
-				}
-			}
-		}
-    	return false;
-	}
+    static public boolean isThreefoldRepetition(List<String> boardStateList, int irreversibleMoveIndex){
+        HashMap<String, Integer> boardStateMap = new HashMap<>();
+        if (boardStateList.size() < 3){
+            return false;
+        }
+        for(String boardState : boardStateList.subList(irreversibleMoveIndex, boardStateList.size()-1)){
+            boardStateMap.putIfAbsent(boardState, 1);
+            if (boardStateMap.containsKey(boardState)){
+                if (boardStateMap.get(boardState) == 2) {
+                    return true;
+                }
+                else{
+                    boardStateMap.replace(boardState, boardStateMap.get(boardState)+1);
+                }
+            }
+        }
+        return false;
+    }
 
-	public static boolean isDeadPosition(Board board){
-    	//going under the assumption that the pieces always contain the king
-    	List<Piece> whitePieces = board.getWhitePieces();
-    	List<Piece> blackPieces = board.getBlackPieces();
+    public static boolean isDeadPosition(Board board){
+        //going under the assumption that the pieces always contain the king
+        List<Piece> whitePieces = board.getWhitePieces();
+        List<Piece> blackPieces = board.getBlackPieces();
 
-    	int whitePiecesSize = whitePieces.size();
-    	int blackPiecesSize = blackPieces.size();
+        int whitePiecesSize = whitePieces.size();
+        int blackPiecesSize = blackPieces.size();
 
-    	Piece whiteBishop = null;
-		Piece blackBishop= null;
-		Piece whiteKnight = null;
-		Piece blackKnight = null;
+        Piece whiteBishop = null;
+        Piece blackBishop= null;
+        Piece whiteKnight = null;
+        Piece blackKnight = null;
 
-		if (whitePiecesSize > 2 || blackPiecesSize > 2){
-			return false;
-		}
+        if (whitePiecesSize > 2 || blackPiecesSize > 2){
+            return false;
+        }
 
-		for(Piece piece : whitePieces){
-			if (piece instanceof Bishop){
-				whiteBishop = piece;
-			}
-			else if (piece instanceof Knight){
-				whiteKnight = piece;
-			}
-		}
+        for(Piece piece : whitePieces){
+            if (piece instanceof Bishop){
+                whiteBishop = piece;
+            }
+            else if (piece instanceof Knight){
+                whiteKnight = piece;
+            }
+        }
 
-		for(Piece piece : blackPieces){
-			if (piece instanceof Bishop){
-				blackBishop = piece;
-			}
-			else if (piece instanceof Knight){
-				blackKnight = piece;
-			}
-		}
+        for(Piece piece : blackPieces){
+            if (piece instanceof Bishop){
+                blackBishop = piece;
+            }
+            else if (piece instanceof Knight){
+                blackKnight = piece;
+            }
+        }
 
-    	if (whitePiecesSize == 1 && whitePieces.get(0) instanceof King){
-			if (blackPiecesSize == 1 && blackPieces.get(0) instanceof King){
-				//king vs king
-				return true;
-			}
-			else if (blackPiecesSize == 2){
-				if (blackKnight != null || blackBishop != null){
-					//king vs king and bishop
-					//king vs king and knight
-					return true;
-				}
-			}
-		}
-    	else if (whitePiecesSize == 2 && blackPiecesSize == 2){
-			if (whiteBishop != null && blackBishop != null){
-				boolean whiteBishopTileColor = (whiteBishop.getRank() + whiteBishop.getFile()) % 2 == 0;
-				boolean blackBishopTileColor = (whiteBishop.getRank() + whiteBishop.getFile()) % 2 == 0;
+        if (whitePiecesSize == 1 && whitePieces.get(0) instanceof King){
+            if (blackPiecesSize == 1 && blackPieces.get(0) instanceof King){
+                //king vs king
+                return true;
+            }
+            else if (blackPiecesSize == 2){
+                if (blackKnight != null || blackBishop != null){
+                    //king vs king and bishop
+                    //king vs king and knight
+                    return true;
+                }
+            }
+        } else if (whitePiecesSize == 2 && blackPiecesSize == 2) {
+            if (whiteBishop != null && blackBishop != null) {
+                boolean whiteBishopTileColor = (whiteBishop.getRank() + whiteBishop.getFile()) % 2 == 0;
+                boolean blackBishopTileColor = (whiteBishop.getRank() + whiteBishop.getFile()) % 2 == 0;
 
-				if (whiteBishopTileColor == blackBishopTileColor){
-					//king and bishop vs king and bishop with same tile color bishops
-					return true;
-				}
-			}
-		}
+                if (whiteBishopTileColor == blackBishopTileColor) {
+                    //king and bishop vs king and bishop with same tile color bishops
+                    return true;
+                }
+            }
+        }
 
-    	if (blackPiecesSize == 1  && whitePiecesSize == 2){
-			if (whiteBishop != null || whiteKnight != null){
-				return true;
-			}
-		}
-		return false;
-	}
+        if (blackPiecesSize == 1 && whitePiecesSize == 2) {
+            if (whiteBishop != null || whiteKnight != null) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
--- chessGame/src/test/java/edu/se181/DrawCheckerTest.java
+++ chessGame/src/test/java/edu/se181/DrawCheckerTest.java
@@ -11,214 +11,214 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class DrawCheckerTest {
-	private DrawChecker drawChecker;
-	private Board board;
+    private DrawChecker drawChecker;
+    private Board board;
 
-	@Before
-	public void beforeEach() {
-		drawChecker = new DrawChecker();
-		board = new Board();
-	}
+    @Before
+    public void beforeEach() {
+        drawChecker = new DrawChecker();
+        board = new Board();
+    }
 
-	@Test
-	public void isIrreversibleMove_GivenPawnMove_Returns1(){
-		Piece piece = new Pawn(1,0, true);
-		Move move = new RegularMove(piece, 2, 0, false);
+    @Test
+    public void isIrreversibleMove_GivenPawnMove_Returns1() {
+        Piece piece = new Pawn(1, 0, true);
+        Move move = new RegularMove(piece, 2, 0, false);
 
-		int result = DrawChecker.isIrreversibleMove(move);
-		Assert.assertEquals(1, result);
-	}
+        int result = DrawChecker.isIrreversibleMove(move);
+        Assert.assertEquals(1, result);
+    }
 
-	@Test
-	public void isIrreversibleMove_GivenMoveWithCapture_Returns1(){
-		Piece piece = new Rook(1,0, true);
-		Move move = new RegularMove(piece, 2, 0, true);
+    @Test
+    public void isIrreversibleMove_GivenMoveWithCapture_Returns1() {
+        Piece piece = new Rook(1, 0, true);
+        Move move = new RegularMove(piece, 2, 0, true);
 
-		int result = DrawChecker.isIrreversibleMove(move);
-		Assert.assertEquals(1, result);
-	}
+        int result = DrawChecker.isIrreversibleMove(move);
+        Assert.assertEquals(1, result);
+    }
 
-	@Test
-	public void isIrreversibleMove_GivenFirstRookMove_Returns2(){
-		Piece piece = new Rook(1,0, true);
-		Move move = new RegularMove(piece, 2, 0, false);
+    @Test
+    public void isIrreversibleMove_GivenFirstRookMove_Returns2() {
+        Piece piece = new Rook(1, 0, true);
+        Move move = new RegularMove(piece, 2, 0, false);
 
-		int result = DrawChecker.isIrreversibleMove(move);
-		Assert.assertEquals(2, result);
-	}
+        int result = DrawChecker.isIrreversibleMove(move);
+        Assert.assertEquals(2, result);
+    }
 
-	@Test
-	public void isIrreversibleMove_GivenNotFirstRookMove_Returns0(){
-		Piece piece = new Rook(1,0, true);
-		piece.hasMoved();
-		Move move = new RegularMove(piece, 2, 0, false);
+    @Test
+    public void isIrreversibleMove_GivenNotFirstRookMove_Returns0() {
+        Piece piece = new Rook(1, 0, true);
+        piece.hasMoved();
+        Move move = new RegularMove(piece, 2, 0, false);
 
-		int result = DrawChecker.isIrreversibleMove(move);
-		Assert.assertEquals(2, result);
-	}
+        int result = DrawChecker.isIrreversibleMove(move);
+        Assert.assertEquals(2, result);
+    }
 
-	@Test
-	public void isIrreversibleMove_GivenCastleMove_Returns2(){
-		King piece = new King(0,4, true);
-		Move move = new CastleMove(piece, true);
+    @Test
+    public void isIrreversibleMove_GivenCastleMove_Returns2() {
+        King piece = new King(0, 4, true);
+        Move move = new CastleMove(piece, true);
 
-		int result = DrawChecker.isIrreversibleMove(move);
-		Assert.assertEquals(2, result);
-	}
+        int result = DrawChecker.isIrreversibleMove(move);
+        Assert.assertEquals(2, result);
+    }
 
-	@Test
-	public void isIrreversibleMove_GivenReversibleMove_Returns0(){
-		Piece piece = new Queen(0,3, true);
-		Move move = new RegularMove(piece, 1,3, false);
+    @Test
+    public void isIrreversibleMove_GivenReversibleMove_Returns0() {
+        Piece piece = new Queen(0, 3, true);
+        Move move = new RegularMove(piece, 1, 3, false);
 
-		int result = DrawChecker.isIrreversibleMove(move);
-		Assert.assertEquals(0, result);
-	}
+        int result = DrawChecker.isIrreversibleMove(move);
+        Assert.assertEquals(0, result);
+    }
 
-	@Test
-	public void isThreefoldRepetition_GivenNoRepetitionAndFullList_ReturnsFalse(){
-		List<String> boardStateList = new ArrayList<String>();
-		boardStateList.add("string1");
-		boardStateList.add("string2");
-		boardStateList.add("string3");
+    @Test
+    public void isThreefoldRepetition_GivenNoRepetitionAndFullList_ReturnsFalse() {
+        List<String> boardStateList = new ArrayList<String>();
+        boardStateList.add("string1");
+        boardStateList.add("string2");
+        boardStateList.add("string3");
 
-		int index = 0;
+        int index = 0;
 
-		boolean result = DrawChecker.isThreefoldRepetition(boardStateList, index);
+        boolean result = DrawChecker.isThreefoldRepetition(boardStateList, index);
 
-		Assert.assertFalse(result);
-	}
+        Assert.assertFalse(result);
+    }
 
-	@Test
-	public void isThreefoldRepetition_GivenRepetitionAndFullList_ReturnsTrue(){
-		List<String> boardStateList = new ArrayList<String>();
-		boardStateList.add("string1");
-		boardStateList.add("string2");
-		boardStateList.add("string3");
-		boardStateList.add("string3");
-		boardStateList.add("string3");
+    @Test
+    public void isThreefoldRepetition_GivenRepetitionAndFullList_ReturnsTrue() {
+        List<String> boardStateList = new ArrayList<String>();
+        boardStateList.add("string1");
+        boardStateList.add("string2");
+        boardStateList.add("string3");
+        boardStateList.add("string3");
+        boardStateList.add("string3");
 
-		int index = 0;
+        int index = 0;
 
-		boolean result = DrawChecker.isThreefoldRepetition(boardStateList, index);
+        boolean result = DrawChecker.isThreefoldRepetition(boardStateList, index);
 
-		Assert.assertTrue(result);
-	}
+        Assert.assertTrue(result);
+    }
 
-	@Test
-	public void isThreefoldRepetition_GivenRepetitionAndSubList_ReturnsTrue(){
-		List<String> boardStateList = new ArrayList<String>();
-		boardStateList.add("string1");
-		boardStateList.add("string2");
-		boardStateList.add("string3");
-		boardStateList.add("string3");
-		boardStateList.add("string3");
+    @Test
+    public void isThreefoldRepetition_GivenRepetitionAndSubList_ReturnsTrue() {
+        List<String> boardStateList = new ArrayList<String>();
+        boardStateList.add("string1");
+        boardStateList.add("string2");
+        boardStateList.add("string3");
+        boardStateList.add("string3");
+        boardStateList.add("string3");
 
-		int index = 2;
+        int index = 2;
 
-		boolean result = DrawChecker.isThreefoldRepetition(boardStateList, index);
+        boolean result = DrawChecker.isThreefoldRepetition(boardStateList, index);
 
-		Assert.assertTrue(result);
-	}
+        Assert.assertTrue(result);
+    }
 
-	@Test
-	public void isDeadPosition_GivenKingVsKing_returnsTrue(){
-		board.loadBoard("00000000000k0000000000000000000000000000000000000K00000000000000000000");
-		// https://lichess.org/editor/5k2/8/8/8/8/3K4/8/8_b_-_-_0_1
+    @Test
+    public void isDeadPosition_GivenKingVsKing_returnsTrue() {
+        board.loadBoard("00000000000k0000000000000000000000000000000000000K00000000000000000000");
+        // https://lichess.org/editor/5k2/8/8/8/8/3K4/8/8_b_-_-_0_1
 
-		boolean result = drawChecker.isDeadPosition(board);
+        boolean result = drawChecker.isDeadPosition(board);
 
-		Assert.assertTrue(result);
-	}
+        Assert.assertTrue(result);
+    }
 
-	@Test
-	public void isDeadPosition_GivenKingRookVsKing_returnsFalse(){
-		board.loadBoard("00000000000k000r000000000000000000000000000000000K00000000000000000000");
-		// https://lichess.org/editor/5k2/1r6/8/8/8/3K4/8/8_b_-_-_0_1
+    @Test
+    public void isDeadPosition_GivenKingRookVsKing_returnsFalse() {
+        board.loadBoard("00000000000k000r000000000000000000000000000000000K00000000000000000000");
+        // https://lichess.org/editor/5k2/1r6/8/8/8/3K4/8/8_b_-_-_0_1
 
-		boolean result = drawChecker.isDeadPosition(board);
+        boolean result = drawChecker.isDeadPosition(board);
 
-		Assert.assertFalse(result);
-	}
+        Assert.assertFalse(result);
+    }
 
-	@Test
-	public void isDeadPosition_GivenKingKnightVsKing_returnsTrue(){
-		board.loadBoard("00000000000k000n000000000000000000000000000000000K00000000000000000000");
-		// https://lichess.org/editor/5k2/1n6/8/8/8/3K4/8/8_b_-_-_0_1
+    @Test
+    public void isDeadPosition_GivenKingKnightVsKing_returnsTrue() {
+        board.loadBoard("00000000000k000n000000000000000000000000000000000K00000000000000000000");
+        // https://lichess.org/editor/5k2/1n6/8/8/8/3K4/8/8_b_-_-_0_1
 
-		boolean result = drawChecker.isDeadPosition(board);
+        boolean result = drawChecker.isDeadPosition(board);
 
-		Assert.assertTrue(result);
-	}
+        Assert.assertTrue(result);
+    }
 
-	@Test
-	public void isDeadPosition_GivenKingBishopVsKing_returnsTrue(){
-		board.loadBoard("00000000000k000b000000000000000000000000000000000K00000000000000000000");
-		// https://lichess.org/editor/5k2/1b6/8/8/8/3K4/8/8_b_-_-_0_1
+    @Test
+    public void isDeadPosition_GivenKingBishopVsKing_returnsTrue() {
+        board.loadBoard("00000000000k000b000000000000000000000000000000000K00000000000000000000");
+        // https://lichess.org/editor/5k2/1b6/8/8/8/3K4/8/8_b_-_-_0_1
 
-		boolean result = drawChecker.isDeadPosition(board);
+        boolean result = drawChecker.isDeadPosition(board);
 
-		Assert.assertTrue(result);
-	}
+        Assert.assertTrue(result);
+    }
 
-	@Test
-	public void isDeadPosition_GivenKingVsKingKnight_returnsTrue(){
-		board.loadBoard("00000000000k000N000000000000000000000000000000000K00000000000000000000");
-		// https://lichess.org/editor/5k2/1N6/8/8/8/3K4/8/8_b_-_-_0_1
+    @Test
+    public void isDeadPosition_GivenKingVsKingKnight_returnsTrue() {
+        board.loadBoard("00000000000k000N000000000000000000000000000000000K00000000000000000000");
+        // https://lichess.org/editor/5k2/1N6/8/8/8/3K4/8/8_b_-_-_0_1
 
-		boolean result = drawChecker.isDeadPosition(board);
+        boolean result = drawChecker.isDeadPosition(board);
 
-		Assert.assertTrue(result);
-	}
+        Assert.assertTrue(result);
+    }
 
-	@Test
-	public void isDeadPosition_GivenKingVsKingBishop_returnsTrue(){
-		board.loadBoard("00000000000k000B000000000000000000000000000000000K00000000000000000000");
-		// https://lichess.org/editor/5k2/1B6/8/8/8/3K4/8/8_b_-_-_0_1
+    @Test
+    public void isDeadPosition_GivenKingVsKingBishop_returnsTrue() {
+        board.loadBoard("00000000000k000B000000000000000000000000000000000K00000000000000000000");
+        // https://lichess.org/editor/5k2/1B6/8/8/8/3K4/8/8_b_-_-_0_1
 
-		boolean result = drawChecker.isDeadPosition(board);
+        boolean result = drawChecker.isDeadPosition(board);
 
-		Assert.assertTrue(result);
-	}
+        Assert.assertTrue(result);
+    }
 
-	@Test
-	public void isDeadPosition_GivenWhiteMoreThan2Pieces_returnsFalse(){
-		board.loadBoard("00000000000k000BB00000000000000000000000000000000K00000000000000000000");
-		// https://lichess.org/editor/5k2/1BB5/8/8/8/3K4/8/8_b_-_-_0_1
+    @Test
+    public void isDeadPosition_GivenWhiteMoreThan2Pieces_returnsFalse() {
+        board.loadBoard("00000000000k000BB00000000000000000000000000000000K00000000000000000000");
+        // https://lichess.org/editor/5k2/1BB5/8/8/8/3K4/8/8_b_-_-_0_1
 
-		boolean result = drawChecker.isDeadPosition(board);
+        boolean result = drawChecker.isDeadPosition(board);
 
-		Assert.assertFalse(result);
-	}
+        Assert.assertFalse(result);
+    }
 
-	@Test
-	public void isDeadPosition_GivenBlackMoreThan2Pieces_returnsFalse(){
-		board.loadBoard("00000000000k000bb00000000000000000000000000000000K00000000000000000000");
-		// https://lichess.org/editor/5k2/1bb5/8/8/8/3K4/8/8_b_-_-_0_1
+    @Test
+    public void isDeadPosition_GivenBlackMoreThan2Pieces_returnsFalse() {
+        board.loadBoard("00000000000k000bb00000000000000000000000000000000K00000000000000000000");
+        // https://lichess.org/editor/5k2/1bb5/8/8/8/3K4/8/8_b_-_-_0_1
 
-		boolean result = drawChecker.isDeadPosition(board);
+        boolean result = drawChecker.isDeadPosition(board);
 
-		Assert.assertFalse(result);
-	}
+        Assert.assertFalse(result);
+    }
 
-	@Test
-	public void isDeadPosition_GivenKingBishopVsKingBishopSameColors_returnsTrue(){
-		board.loadBoard("00000000000k000b0B0000000000000000000000000000000K00000000000000000000");
-		// https://lichess.org/editor/5k2/1b1B4/8/8/8/3K4/8/8_b_-_-_0_1
+    @Test
+    public void isDeadPosition_GivenKingBishopVsKingBishopSameColors_returnsTrue() {
+        board.loadBoard("00000000000k000b0B0000000000000000000000000000000K00000000000000000000");
+        // https://lichess.org/editor/5k2/1b1B4/8/8/8/3K4/8/8_b_-_-_0_1
 
-		boolean result = drawChecker.isDeadPosition(board);
+        boolean result = drawChecker.isDeadPosition(board);
 
-		Assert.assertTrue(result);
-	}
+        Assert.assertTrue(result);
+    }
 
-	@Test
-	public void isDeadPosition_GivenKingBishopVsKingBishopDifferentColors_returnsFalse(){
-		board.loadBoard("00000000000k000bB00000000000000000000000000000000K00000000000000000000");
-		// https://lichess.org/editor/5k2/1bB5/8/8/8/3K4/8/8_b_-_-_0_1
+    @Test
+    public void isDeadPosition_GivenKingBishopVsKingBishopDifferentColors_returnsFalse() {
+        board.loadBoard("00000000000k000bB00000000000000000000000000000000K00000000000000000000");
+        // https://lichess.org/editor/5k2/1bB5/8/8/8/3K4/8/8_b_-_-_0_1
 
-		boolean result = drawChecker.isDeadPosition(board);
+        boolean result = drawChecker.isDeadPosition(board);
 
-		Assert.assertTrue(result);
-	}
+        Assert.assertTrue(result);
+    }
 
 }
